2f14931967d6c05b72bf2c7aaa86060123e7b580
rafaelcbc <comerciorafael@hotmail.com>
2016-12-11 22:56:57 -0200

    First commit.

---
+++ RDSearch/RDSearch.DomainObjects/Entities/Perfil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDSearch.DomainObjects.Entities
+{
+    class Perfil
+    {
+        public int PerfilId { get; set; }
+        public string Tipo { get; set; }
+    }
+}
---
+++ RDSearch/RDSearch.DomainObjects/Entities/Sala.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDSearch.DomainObjects.Entities
+{
+    class Sala
+    {
+        public int SalaId { get; set; }
+        public int Numero { get; set; }
+    }
+}
